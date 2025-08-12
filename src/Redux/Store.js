269d2c1d9bce975a107baa052b29816bfce496ea
faiyaz alam <faiyaz0399@gmail.com>
2025-08-12 17:30:34 +0530

    adding basic  redux setup

---
+++ src/Redux/Store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducer from './Slices/AuthSlice'
+
+export const store = configureStore({
+  reducer: {
+	auth : authSliceReducer
+  },
+  devTools: true
+})
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import './index.css'
-
+import App from './App.jsx'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-
-import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+import { store } from './Redux/Store.js'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-    <T
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
 )
