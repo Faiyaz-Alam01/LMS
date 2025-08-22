304cda95e939b7194dae7cbeb9db260ec73be92c
faiyaz alam <faiyaz0399@gmail.com>
2025-08-22 17:40:09 +0530

    Updated profile

--- src/Pages/User/Profile.jsx
+++ src/Pages/User/Profile.jsx
@@ -1,32 +1,51 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import HomeLayouts from '../../Layouts/HomeLayouts';
 import { FaRegUserCircle } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import axiosInstance from '../../Helpers/axiosInstance';
 
 
 const Profile = () => {
 
-	const userdata = useSelector((state) => state?.auth?.data);
+  	const [users, setUsers] = useState([]);
+	console.log(users);
+	const userData = users?.data
+
+	useEffect(()=>{
+		const fetchUsers = async () => {
+			try {
+				const res = await axiosInstance.get('/user/get-user')
+				const data = res.data;
+				if(data.success){
+					setUsers(data);
+				}
+
+			} catch (error) {
+				console.error("Error fetching users:", error);
+			}
+		}
+		fetchUsers();
+	},[])
 
 	return (
 		<HomeLayouts>
 			<div className='min-h-[90vh] flex items-center justify-center'>
 				<div className='my-10 flex flex-col gap-4 rounded-lg p-4 text-white w-md shadow-[0_0_10px_black]'>
 					<img 
-						src={userdata?.avatar?.secure_url || <FaRegUserCircle /> } 
-						className='size-22 rounded-full m-auto border border-black'
+						src={userData?.avatar || <FaRegUserCircle/> } 
+						className='w-22 h-22 text-center rounded-full m-auto border border-black'
 					/>
 					<h3 className='text-xl font-semibold text-center capitalize'>
-						{userdata?.fullName || "Faiyaz"}
+						{userData?.fullName || "Faiyaz"}
 					</h3>
-					<div className='grid grid-cols-2'>
-						<p>Email</p><p>{userdata?.email}</p>
+					<div className='grid grid-cols-2 gap-y-2'>
+						<p>Email</p><p>{userData?.email}</p>
 						
-						<p>Role</p><p>{userdata?.role}</p>
+						<p>Role</p><p>{userData?.role}</p>
 						
 						<p>Subscription :</p>
-						<p>{userdata?.subscription?.status === "active"? "action": "inactive"}</p>
+						<p>{userData?.subscription?.status === "active"? "action": "inactive"}</p>
 
 					</div>
 
@@ -43,11 +62,11 @@ const Profile = () => {
 						</Link>
 					</div>
 					{/* {/* .status === "active" && * change during backend setup/} */}
-					{userdata?.subscription?.status !== "active" && (
+					{/* {userdata?.subscription?.status !== "active" && ( */}
 						<button className='py-1 font-medium w-full bg-red-600 hover:bg-red-500 transition-all ease-in-out duration-200'>
 							Cancle Subscription
 						</button>
-					)}
+					{/* )} */}
 				</div>
 			</div>
 		</HomeLayouts>
