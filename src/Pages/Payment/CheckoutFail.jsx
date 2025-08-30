65da1ed4707ef8920a27dfd71335f6a033381c09
faiyaz alam <faiyaz0399@gmail.com>
2025-08-30 22:29:44 +0530

    Added Checkout Fail page

--- src/Pages/Payment/CheckoutFail.jsx
+++ src/Pages/Payment/CheckoutFail.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import HomeLayouts from '../../Layouts/HomeLayouts'
+import { RxCrossCircled } from 'react-icons/rx'
+import { Link } from 'react-router-dom'
 
 const CheckoutFail = () => {
-  return (
-	<div>
-	  
-	</div>
-  )
+
+	return (
+		<HomeLayouts>
+			<div className='flex justify-center items-center min-h-[90vh] text-white'>
+				<div className='w-80  h-[26rem] flex flex-col justify-center items-center shadow-[0_0_10px_black] rounded-lg relative'>
+					<h1 className='bg-red-500 absolute top-0 w-full  py-4 text-2xl font-bold text-center rounded-tl-lg rounded-tr-lg'>Payment Failed</h1>
+
+					<div className='px-4 flex flex-col items-center justify-center space-y-2'>
+						<div className='text-lg space-y-2 text-center'>
+							<h2>Oops ! Your payment failed</h2>
+							<p className='text-left'>
+								please try again later
+							</p>
+						</div>
+						<RxCrossCircled className='text-red-500 text-5xl' />
+					</div>
+					<Link to={'/checkout'} className='bg-red-500 hover:bg-red-600 transition-all ease-in-out duration-300 absolute bottom-0 w-full py-2 text-xl font-extrabold text-center rounded-bl-lg rounded-br-lg '>
+						<button>Try again</button>
+					</Link>
+				</div>
+			</div>
+		
+		</HomeLayouts>
+	)
 }
 
 export default CheckoutFail
