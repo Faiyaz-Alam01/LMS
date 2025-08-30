b35c4774e99ad734272e39050f83d7da0accd0d7
faiyaz alam <faiyaz0399@gmail.com>
2025-08-30 22:44:13 +0530

    changed subscription status

--- src/Pages/Course/CourseDescription.jsx
+++ src/Pages/Course/CourseDescription.jsx
@@ -41,7 +41,7 @@ const CourseDescription = () => {
 								{state?.instructor}
 							</p>
 						</div>
-						{role === 'ADMIN' || user?.subscription?.status === 'ACTIVE' ? (
+						{role === 'ADMIN' || user?.subscription?.status === 'active' ? (
 								<button onClick={() => navigate('/course/displaylecture', {state: {...state}})} className='bg-yellow-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-yellow-500 transition-all ease-in-out duration-300'>
 									Watch lectures
 								</button>
